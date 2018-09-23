37edd66eb3497d2b67e38c1912b767fa8953f8e6
nightman20104 <programist20104@gmail.com>
2018-09-22 17:40:09 -0700

    Exercise 3

--- src/index.js
+++ src/index.js
@@ -1,30 +1,48 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.massiv = [];
   }
 
   add(element) {
-    // your implementation
+    this.massiv.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.massiv[index];
   }
 
   get length() {
-    // your implementation
+    return this.massiv.length;
   }
 
   toArray() {
-    // your implementation
+    return this.massiv;
   }
 
   sort(indices) {
-    // your implementation
+      indices.sort();
+      let temporaryMassiv = [];
+      function compareNumbers(a, b) {
+        return a - b;
+      }
+      for (let i = 0; i < indices.length; i++)  {
+        temporaryMassiv[i] = this.massiv[indices[i]];
+      }
+      if (this.compareFunction)  {
+        temporaryMassiv.sort(this.compareFunction);
+      }
+      else {
+        temporaryMassiv.sort(compareNumbers);
+      }
+      let element = 0;
+      for (let i = 0; i < indices.length; i++) {
+        this.massiv[indices[i]] = temporaryMassiv[element];
+        element++;
+      }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
